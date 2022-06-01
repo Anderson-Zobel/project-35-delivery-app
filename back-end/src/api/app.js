69e1f70e1be1713136177ce3f39a482f0f6cbaf0
Emerson Oliveira <emerson.contato@gmail.com>
2022-06-01 18:28:30 -0300

    Adiciona endpoint de Login

--- back-end/src/api/app.js
+++ back-end/src/api/app.js
@@ -1,7 +1,14 @@
 const express = require('express');
+// multer
+const LoginService = require('./services/loginService');
+
+const loginRoute = new LoginService();
 
 const app = express();
 
-app.get('/coffee', (_req, res) => res.status(418).end());
+app.get('/login',
+loginRoute.getUser,
+(_req, res) => res.status(418).end());
+
 
 module.exports = app;
