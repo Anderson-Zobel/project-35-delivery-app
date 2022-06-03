fcaa9b2f58d5fda2d78387b25a7acbda80d25c57
Rodrigo Lima <limarodrigoo@outlook.com>
2022-06-03 18:47:25 -0300

    implementa requisição axios

--- front-end/src/shared/services/api/index.js
+++ front-end/src/shared/services/api/index.js
@@ -1,12 +1,13 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'https://localhost:3009',
+  baseURL: 'http://localhost:3009',
 });
 
 const getUser = async ({ email, password }) => {
   try {
     const { data } = await api.post('/login', { email, password });
+    console.log(data);
     return data;
   } catch (error) {
     console.log(JSON.stringify(error));
