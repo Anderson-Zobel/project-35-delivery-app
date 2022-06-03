aac256ac0b78cb5c7b01091760903aad095ac5ce
Pablo Almeida <pabloalmeidac@hotmail.com>
2022-06-03 17:20:27 -0300

    Co-authored-by: Anderson Zobel <Anderson-Zobel@users.noreply.github.com> Co-authored-by: Lucas Felipe Ribeiro Fernandes <lucaslol69@users.noreply.github.com>

--- back-end/src/api/app.js
+++ back-end/src/api/app.js
@@ -1,11 +1,14 @@
 const express = require('express');
 require('express-async-errors');
+const cors = require('cors');
 const errorMiddleware = require('./middlewares/error');
 const loginRoute = require('./routes/userRoutes');
 
 const app = express();
 
 app.use(express.json());
+
+app.use(cors());
 
 app.use('/', loginRoute);
 
