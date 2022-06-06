300b0a67c56ff9f3acd4043357c8704e68716dd7
Rodrigo Lima <limarodrigoo@outlook.com>
2022-06-06 17:24:16 -0300

    UPDATE:? usa env para acesssar API

--- front-end/src/shared/services/api/apiConfig.js
+++ front-end/src/shared/services/api/apiConfig.js
@@ -1,5 +1,8 @@
 import axios from 'axios';
 
+const PORT = process.env.REACT_APP_HOST_API || 'http://localhost:3001';
+
+
 export default apiConfig = () => axios.create({
-  baseURL: 'http://localhost:3009',
+  baseURL: PORT,
 });
