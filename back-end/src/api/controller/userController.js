f477372efc54d15b06062d0e4b3d91f4d803be3c
Emerson Oliveira <emersonoliveira.contato@gmail.com>
2022-06-07 17:50:31 -0300

    Alteração de noma da função

--- back-end/src/api/controller/userController.js
+++ back-end/src/api/controller/userController.js
@@ -1,12 +1,25 @@
-const userService = require('../services/userService');
+const { loginService, createUser } = require('../services/userService'); 
 
 const login = async (req, res, _next) => {
     const { email, password } = req.body;
-    const result = await userService(email, password);
+    const result = await loginService(email, password);
     if (!result) {
         return res.status(404).json(null);
     }
     return res.status(200).json(result);    
 };
 
-module.exports = login;
+const create = async (req, res, _next) => {
+    const { name, email, password, role } = req.body;
+
+    const user = await createUser(name, email, password, role);
+    if (!user) {
+        return res.status(409).json({ error: 'user already exists' });
+    }
+    return res.status(201).json(user);
+}
+
+module.exports = {
+    login,
+    create,
+}
