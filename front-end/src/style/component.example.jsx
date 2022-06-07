7ce26f4a1dee4a3f9ef3074be987c1494aef27a7
Anderson-Zobel <anderson.zobels@gmail.com>
2022-06-07 15:08:32 -0300

    FEAT: Estilizando MUI - Criação de Componentes

---
+++ front-end/src/style/component.example.jsx
@@ -0,0 +1,16 @@
+import { Grid } from '@mui/material';
+
+export default function Gride({ children }) {
+  return(
+    <Grid
+      container
+      spacing={0}
+      direction="column"
+      alignItems="center"
+      justifyContent="center"
+      style={{ minHeight: '100vh' }}
+    >
+      { children }
+    </Grid>
+  )
+}
