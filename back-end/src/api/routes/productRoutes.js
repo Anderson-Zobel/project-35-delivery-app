0103b73a552fea120d050a0e4f68c64f9dbe32fc
Emerson Oliveira <emersonoliveira.contato@gmail.com>
2022-06-08 16:36:28 -0300

    Criação da rota de produtos

---
+++ back-end/src/api/routes/productRoutes.js
@@ -0,0 +1,12 @@
+const { Router } = require('express');
+const productController = require('../controller/productController');
+
+
+const product = Router();
+
+product.get(
+  '/products',
+  productController.getProducts,
+);
+
+module.exports = product;
