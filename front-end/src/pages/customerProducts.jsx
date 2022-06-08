cc1be92b7b650e0049d390b5094394679c3c8550
Rodrigo Lima <limarodrigoo@outlook.com>
2022-06-08 18:29:27 -0300

    arruma lint

--- front-end/src/pages/customerProducts.jsx
+++ front-end/src/pages/customerProducts.jsx
@@ -1,14 +1,13 @@
-import React, { useState } from 'react';
-import { Container } from '@mui/material'
-import NavBar from '../shared/components/navBar'
-import CardDrinks from '../shared/components/cardDrinks'
+import React from 'react';
+import NavBar from '../shared/components/navBar';
+// import CardDrinks from '../shared/components/cardDrinks';
 
 export default function CustomerProducts() {
-  return ( 
-    <NavBar/>
+  return (
+    <NavBar />
     // <Container>
     //   <p>botao  - botao pedido - Usuario - Botao Logout</p>
     //   <CardDrinks/>
-    // </Container> 
-  )
-};
+    // </Container>
+  );
+}
--- front-end/src/shared/components/cardDrinks.jsx
+++ front-end/src/shared/components/cardDrinks.jsx
@@ -1,7 +1,7 @@
-import React, { useState } from 'react';
+import React from 'react';
 
-export default function CardDrinks () {
-    return (
-        <div>bebida</div>
-    )
-};
+export default function CardDrinks() {
+  return (
+    <div>bebida</div>
+  );
+}
