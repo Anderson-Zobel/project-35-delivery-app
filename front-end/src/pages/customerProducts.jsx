0c2d1715f938c2a7a42de0bb8af493fe79cb5c37
Rodrigo Lima <limarodrigoo@outlook.com>
2022-06-09 14:36:29 -0300

    implemente chamada da api para produtos

--- front-end/src/pages/customerProducts.jsx
+++ front-end/src/pages/customerProducts.jsx
@@ -1,8 +1,19 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import NavBar from '../shared/components/navBar';
+import { getProducts } from '../shared/services/api';
 // import CardDrinks from '../shared/components/cardDrinks';
 
 export default function CustomerProducts() {
+  const [products, setProducts] = useState([]);
+
+  useEffect(() => {
+    async function fetchAPI() {
+      const response = await getProducts();
+      setProducts(response);
+    }
+    fetchAPI();
+  }, []);
+  console.log(products, 'estado');
   return (
     <NavBar />
     // <Container>
