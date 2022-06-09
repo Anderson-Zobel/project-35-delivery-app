03766b66c3831bb9a012e82798e39c7e1ea5e041
Anderson-Zobel <anderson.zobels@gmail.com>
2022-06-09 18:56:33 -0300

    FEAT: Adiciona data-testid

--- front-end/src/shared/components/navBar.jsx
+++ front-end/src/shared/components/navBar.jsx
@@ -15,23 +15,42 @@ export default function NavBar() {
       <Toolbar sx={ { justifyContent: 'space-between' } }>
         <Stack direction="row" spacing={ 2 } sx={ { flexGrow: 1 } }>
           <Button color="inherit">
-            <Link href="/customer/products" color="inherit" underline="none">
+            <Link
+              href="/customer/products"
+              color="inherit"
+              underline="none"
+              data-testid="customer_products__element-navbar-link-products"
+            >
               Produtos
             </Link>
           </Button>
           <Button color="inherit">
-            <Link href="/orders" color="inherit" underline="none">
+            <Link
+              href="/orders"
+              color="inherit"
+              underline="none"
+              data-testid="customer_products__element-navbar-link-orders"
+            >
               Pedidos
             </Link>
           </Button>
         </Stack>
-        <Typography sx={ { fontWeight: 'bold' } } component="div">
+        <Typography
+          sx={ { fontWeight: 'bold' } }
+          component="div"
+          data-testid="customer_products__element-navbar-user-full-name"
+        >
           {' '}
           Nome Cliente
           {' '}
         </Typography>
         <Button color="inherit" sx={ { pl: 3 } }>
-          <Link href="/login" color="inherit" underline="none">
+          <Link
+            href="/login"
+            color="inherit"
+            underline="none"
+            data-testid="customer_products__element-navbar-link-logout"
+          >
             Logout
           </Link>
         </Button>
