dbdaa4d9e3949363f8f6c1d0b209e29b0e3d1e26
Anderson-Zobel <anderson.zobels@gmail.com>
2022-06-09 18:54:59 -0300

    FEAT: Adiciona contador, data-testid

--- front-end/src/shared/components/cardDrinks.jsx
+++ front-end/src/shared/components/cardDrinks.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import PropTypes from 'prop-types';
 import {
   Card,
@@ -12,14 +12,28 @@ import {
 } from '@mui/material';
 
 export default function CardDrinks({ product }) {
-  console.log(product);
-  const { name, urlImage, price } = product;
-  console.log(name);
+  const { id, name, urlImage, price } = product;
+  const [count, setCount] = useState(0);
+
+  function incrementCount() {
+    const countHandler = count + 1;
+    setCount(countHandler);
+  }
+  function decrementCount() {
+    const countHandler = count - 1;
+    setCount(countHandler);
+  }
+
+  function handleChange({ target }) {
+    setCount(+target.value);
+  }
+
   return (
-    <Card variant="outlined" sx={ { maxWidth: 345, display: 'inline-block', m: '1rem' } }>
+    <Card variant="outlined" sx={ { width: 345, display: 'inline-block', m: '1rem' } }>
       <CardHeader
         title={ name }
         sx={ { textAlign: 'center' } }
+        data-testid={ `customer_products__element-card-title-${id}` }
       />
 
       <CardMedia
@@ -28,18 +42,37 @@ export default function CardDrinks({ product }) {
         image={ urlImage }
         alt="Cervejinha"
         sx={ { objectFit: 'scale-down' } }
+        data-testid={ `customer_products__img-card-bg-image-${id}` }
       />
       <CardContent>
         <Typography
           variant="h6"
           sx={ { textAlign: 'center' } }
+          data-testid={ `customer_products__element-card-price-${id}` }
         >
           { ` R$ ${price} ` }
         </Typography>
-        <ButtonGroup>
-          <Button>-</Button>
-          <TextField width="50" />
-          <Button>+</Button>
+        <ButtonGroup sx={ { display: 'flex', justifyContent: 'center' } }>
+          <Button
+            data-testid={ `customer_products__button-card-rm-item-${id}` }
+            onClick={ () => decrementCount() }
+          >
+            -
+          </Button>
+          <TextField
+            sx={ { width: 50 } }
+            value={ count }
+            inputProps={ {
+              onChange: handleChange,
+              'data-testid': `customer_products__input-card-quantity-${id}`,
+            } }
+          />
+          <Button
+            data-testid={ `customer_products__button-card-add-item-${id}` }
+            onClick={ () => incrementCount() }
+          >
+            +
+          </Button>
         </ButtonGroup>
       </CardContent>
     </Card>
