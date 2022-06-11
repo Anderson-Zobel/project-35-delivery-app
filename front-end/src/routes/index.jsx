a8f6aaeae7ddb9a4127f6e76edc8260530289c1b
Anderson-Zobel <anderson.zobels@gmail.com>
2022-06-11 18:39:20 -0300

    FEAT: Cria rota customer/checkout

--- front-end/src/routes/index.jsx
+++ front-end/src/routes/index.jsx
@@ -3,6 +3,7 @@ import { Routes, Route, Navigate } from 'react-router-dom';
 import Login from '../pages/login';
 import Register from '../pages/register';
 import CustomerProducts from '../pages/customerProducts';
+import CustomerCheckout from '../pages/customerCheckout'
 
 export default function AppRoutes() {
   return (
@@ -11,6 +12,7 @@ export default function AppRoutes() {
       <Route path="/" element={ <Navigate to="/login" /> } />
       <Route path="/register" element={ <Register /> } />
       <Route path="/customer/products" element={ <CustomerProducts /> } />
+      <Route path="/customer/checkout" element={ <CustomerCheckout /> } />
     </Routes>
   );
 }
