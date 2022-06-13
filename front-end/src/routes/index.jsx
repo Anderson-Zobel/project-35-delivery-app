c12baf01b69040ea89209c3b5848613b6bcee109
Anderson-Zobel <anderson.zobels@gmail.com>
2022-06-13 17:07:10 -0300

    FEAT: adicionando rota customer/orders

--- front-end/src/routes/index.jsx
+++ front-end/src/routes/index.jsx
@@ -3,7 +3,8 @@ import { Routes, Route, Navigate } from 'react-router-dom';
 import Login from '../pages/login';
 import Register from '../pages/register';
 import CustomerProducts from '../pages/customerProducts';
-import CustomerCheckout from '../pages/customerCheckout'
+import CustomerCheckout from '../pages/customerCheckout';
+import CustomerOrderId from '../pages/customerOrderId';
 
 export default function AppRoutes() {
   return (
@@ -13,6 +14,7 @@ export default function AppRoutes() {
       <Route path="/register" element={ <Register /> } />
       <Route path="/customer/products" element={ <CustomerProducts /> } />
       <Route path="/customer/checkout" element={ <CustomerCheckout /> } />
+      <Route path="/customer/orders/:id" element={ <CustomerOrderId /> } />
     </Routes>
   );
 }
