bb9594c4a3af9d69dccd6ce35bd4346224dc81c2
Rodrigo Lima <limarodrigoo@outlook.com>
2022-06-14 15:22:43 -0300

    adiciona sale router ao server

--- back-end/src/api/app.js
+++ back-end/src/api/app.js
@@ -2,7 +2,7 @@ const express = require('express');
 require('express-async-errors');
 const cors = require('cors');
 const errorMiddleware = require('./middlewares/error');
-const { userRoutes, productRoutes } = require('./routes/index');
+const { userRoutes, productRoutes, saleRoutes } = require('./routes/index');
 
 const app = express();
 
@@ -13,6 +13,7 @@ app.use(express.static('public'));
 
 app.use('/', userRoutes);
 app.use('/', productRoutes);
+app.use('/', saleRoutes);
 
 app.use(errorMiddleware);
 
