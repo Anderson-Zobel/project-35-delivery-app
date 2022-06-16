7dfee6babba04734d309d01137082059cb43a449
Anderson-Zobel <anderson.zobels@gmail.com>
2022-06-15 23:27:57 -0300

    FEAT: Tableorder funcional

--- front-end/src/shared/components/TableOrder.jsx
+++ front-end/src/shared/components/TableOrder.jsx
@@ -1,4 +1,4 @@
-import React, { useContext } from 'react';
+import React, { useState, useEffect } from 'react';
 import {
   TableHead,
   TableRow,
@@ -6,66 +6,116 @@ import {
   Table,
   TableContainer,
   TableBody,
+  Typography,
+  Button,
+  Container,
 } from '@mui/material';
-import Context from '../contexts/Context';
+import moment from 'moment';
+import { useParams } from 'react-router-dom';
+import { getOrderById, setOrderStatusById } from '../services/api';
+import TableTitles from './TableTitles';
 
-const testId = 'customer_order_details__element-order-table-';
+const testId = 'customer_order_details__element-order-';
 
 export default function TableOrder() {
-  const { userCart } = useContext(Context);
+  const [order, setOrder] = useState();
+  const [deliveryStatus, setDeliveryStatus] = useState('pendente');
+  const { id } = useParams();
+
+  console.log(order);
+
+  useEffect(() => {
+    async function fetchAPI() {
+      const response = await getOrderById(id);
+      console.log(response);
+      setOrder(response);
+    }
+    fetchAPI();
+  }, [id]);
+
+  const handleClick = async () => {
+    const response = await setOrderStatusById(id);
+    setDeliveryStatus(response)  
+    console.log(response); 
+  };
+  
+
   return (
-    <TableContainer>
-      <Table sx={ { minWidth: 650 } } size="small" aria-label="a dense table">
-        <TableHead>
-          <TableRow>
-            <TableCell>Item</TableCell>
-            <TableCell align="right">Descrição</TableCell>
-            <TableCell align="right">Quantidade</TableCell>
-            <TableCell align="right">Valor Unitário</TableCell>
-            <TableCell align="right">Sub-total</TableCell>
-          </TableRow>
-        </TableHead>
-        <TableBody>
-          {userCart.map((item, index) => (
-            <TableRow
-              key={ userCart.name }
-              sx={ { '&:last-child td, &:last-child th': { border: 0 } } }
-            >
-              <TableCell
-                align="right"
-                data-testid={ `${testId}item-number-${index}` }
+    <Container>
+      <TableContainer>
+        <Table sx={ { minWidth: 650 } } size="small" aria-label="a dense table">
+          <TableHead>
+            { order ? (
+              <Typography>
+                Pedido:
+                <span data-testid={ `${testId}details-label-order-id` }>
+                  { order.id}
+                </span>
+                P.Vendedora:
+                <span data-testid={ `${testId}details-label-seller-name` }>
+                  {order.seller.name}
+                </span>
+                <span data-testid={ `${testId}label-order-date` }>
+                  {moment(order.saleDate).format('DD/MM/YYYY')}
+                </span>
+                <span data-testid={ `${testId}label-delivery-status` }>
+                  {deliveryStatus}
+                </span>
+                <Button
+                  data-testid="customer_order_details__button-delivery-check"
+                  onClick={ () => handleClick() }
+                >
+                  Marcar como entregue
+                </Button>
+              </Typography>) : null }
+
+            <TableTitles />
+
+          </TableHead>
+          <TableBody>
+            { order ? (order.products.map((item, index) => (
+              <TableRow
+                key={ item.name }
+                sx={ { '&:last-child td, &:last-child th': { border: 0 } } }
               >
-                {index + 1}
-              </TableCell>
-              <TableCell
-                component="th"
-                scope="row"
-                data-testid={ `${testId}name-${index}` }
-              >
-                {item.name}
-              </TableCell>
-              <TableCell
-                align="right"
-                data-testid={ `${testId}quantity-${index}` }
-              >
-                {item.count}
-              </TableCell>
-              <TableCell
-                align="right"
-                data-testid={ `${testId}unit-price-${index}` }
-              >
-                {item.price.replace('.', ',')}
-              </TableCell>
-              <TableCell
-                align="right"
-                data-testid={ `${testId}sub-total-${index}` }
-              >
-                {(item.price * item.count).toFixed(2).replace('.', ',')}
-              </TableCell>
-            </TableRow>
-          ))}
-        </TableBody>
-      </Table>
-    </TableContainer>
+                <TableCell
+                  align="right"
+                  data-testid={ `${testId}table-item-number-${index}` }
+                >
+                  {index + 1}
+                </TableCell>
+                <TableCell
+                  component="th"
+                  scope="row"
+                  data-testid={ `${testId}table-name-${index}` }
+                >
+                  {item.name}
+                </TableCell>
+                <TableCell
+                  align="right"
+                  data-testid={ `${testId}table-quantity-${index}` }
+                >
+                  {item.SaleProduct.quantity}
+                </TableCell>
+                <TableCell
+                  align="right"
+                  data-testid={ `${testId}table-unit-price-${index}` }
+                >
+                  {item.price.replace('.', ',')}
+                </TableCell>
+                <TableCell
+                  align="right"
+                  data-testid={ `${testId}table-sub-total-${index}` }
+                >
+                  {(item.SaleProduct.quantity * item.price).toFixed(2).replace('.', ',')}
+                </TableCell>
+              </TableRow>
+            ))) : null }
+          </TableBody>
+        </Table>
+      </TableContainer>
+      <Container>{ order ? (order.totalPrice): null }</Container>
+    </Container>
   );
 }
+ 
