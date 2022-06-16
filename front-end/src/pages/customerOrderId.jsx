0955e209164ea6e7f5d14e3df83b0c852edb6763
Anderson-Zobel <anderson.zobels@gmail.com>
2022-06-16 16:29:40 -0300

    FEAT: Fetch na page Order by id

--- front-end/src/pages/customerOrderId.jsx
+++ front-end/src/pages/customerOrderId.jsx
@@ -1,12 +1,29 @@
-import React from 'react';
+import React, { useContext, useEffect } from 'react';
 import {
   Container,
   Paper,
 } from '@mui/material';
-import NavBar from '../shared/components/NavBar';
-import TableOrder from '../shared/components/TableOrder';
+import { useParams } from 'react-router-dom';
+import NavBar from '../shared/components-pages/NavBar';
+import TableOrder from '../shared/components-pages/order/TableOrder';
+import Context from '../shared/contexts/Context';
+import { getOrderById } from '../shared/services/api';
 
 export default function OrderById() {
+  const { setOrder } = useContext(Context);
+
+  const { id } = useParams();
+  console.log(id);
+
+  useEffect(() => {
+    async function fetchAPI() {
+      const response = await getOrderById(+id);
+      setOrder(response);
+      console.log(response);
+    }
+    fetchAPI();
+  }, [id]);
+
   return (
     <>
       <NavBar />
