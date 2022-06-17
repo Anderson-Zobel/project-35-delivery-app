6fc4c1de6b3d8e6e66b224a1d6b7aab19e0df667
Pablo Almeida <pabloalmeidac@hotmail.com>
2022-06-17 13:24:26 -0300

    feat: deixa dinâmico e cria regras pra desabilitar botões

--- front-end/src/shared/components-pages/order/TableHeadContent.jsx
+++ front-end/src/shared/components-pages/order/TableHeadContent.jsx
@@ -1,4 +1,4 @@
-import React, { useContext } from 'react';
+import React, { useContext, useState, useEffect } from 'react';
 import { useParams } from 'react-router-dom';
 import { Typography, Button, TableHead, TableRow, TableCell } from '@mui/material';
 import moment from 'moment';
@@ -8,13 +8,45 @@ import Context from '../../contexts/Context';
 export default function TableHeadContent() {
   const { order, deliveryStatus, setDeliveryStatus } = useContext(Context);
 
+  const [prepare, setPrepare] = useState(true);
+  const [deliver, setDeliver] = useState(true);
+  const [delivered, setDelivered] = useState(true);
+
   const { id } = useParams();
 
-  const handleClick = async () => {
-    const response = await setOrderStatusById(id);
+  const handleClick = async ({ target }) => {
+    const status = target.value;
+    const response = await setOrderStatusById(id, status);
     setDeliveryStatus(response.sale.status);
   };
-  const testId = 'customer_order_details__element-order-';
+
+  useEffect(() => {
+    async function fetchAPI() {
+      if (deliveryStatus === 'Pendente') {
+        setPrepare(false);
+        setDeliver(true);
+      }
+      if (deliveryStatus === 'Preparando') {
+        setDeliver(false);
+        setPrepare(true);
+      }
+      if (deliveryStatus === 'Em Trânsito') {
+        setDelivered(false);
+        setPrepare(true);
+        setDeliver(true);
+      }
+      if (deliveryStatus === 'Entregue') {
+        setDeliver(true);
+        setDelivered(true);
+        setPrepare(true);
+      }
+    }
+    fetchAPI();
+  }, [deliveryStatus]);
+
+  const { role } = JSON.parse(localStorage.getItem('user'));
+
+  const testId = `${role}_order_details__element-order-`;
 
   return (
     <TableHead>
@@ -30,16 +62,40 @@ export default function TableHeadContent() {
         <span data-testid={ `${testId}details-label-order-date` }>
           {moment(order.saleDate).format('DD/MM/YYYY')}
         </span>
+
         <span data-testid={ `${testId}details-label-delivery-status` }>
           {deliveryStatus}
         </span>
-        <Button
-          data-testid="customer_order_details__button-delivery-check"
-          disabled
-          onClick={ () => handleClick() }
-        >
-          Marcar como entregue
-        </Button>
+        { role === 'customer' ? (
+          <Button
+            data-testid="customer_order_details__button-delivery-check"
+            disabled={ delivered }
+            value="Entregue"
+            onClick={ (e) => handleClick(e) }
+          >
+            Marcar como entregue
+          </Button>
+        ) : null}
+        {role === 'seller' ? (
+          <>
+            <Button
+              data-testid="seller_order_details__button-preparing-check"
+              onClick={ (e) => handleClick(e) }
+              value="Preparando"
+              disabled={ prepare }
+            >
+              PREPARAR PEDIDO
+            </Button>
+            <Button
+              data-testid="seller_order_details__button-dispatch-check"
+              onClick={ (e) => handleClick(e) }
+              value="Em Trânsito"
+              disabled={ deliver }
+            >
+              SAIU PARA ENTREGA
+            </Button>
+          </>
+        ) : null}
       </Typography>
       <TableRow>
         <TableCell>Item</TableCell>
