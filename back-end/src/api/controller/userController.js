8f594b19290a78b4a0c7e0a7a26901706617acf6
Pablo Almeida <pabloalmeidac@hotmail.com>
2022-06-18 14:10:10 -0300

    feat: controller para criar usuario somente com a role admin

--- back-end/src/api/controller/userController.js
+++ back-end/src/api/controller/userController.js
@@ -19,6 +19,20 @@ const create = async (req, res, _next) => {
     return res.status(201).json(user);
 };
 
+const adminCreateUser = async (req, res, _next) => {
+    const token = req.user; 
+    if (token.role !== 'administrator') {
+        return res.status(401).json({ error: 'User not authorized' });
+    }
+    const { name, email, password, role } = req.body;
+    
+    const user = await createUser(name, email, password, role);
+    if (!user) {
+        return res.status(409).json({ error: 'user already exists' });
+    }
+    return res.status(201).json(user);
+};
+
 const getAllUsers = async (req, res, _next) => {
     const users = await getUsers();
     if (!users) {
@@ -40,4 +54,5 @@ module.exports = {
     create,
     getSellers,
     getAllUsers,
+    adminCreateUser,
 };
