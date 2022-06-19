c418319e37f5864af120e689f8bbf6eb5bb03c95
Anderson-Zobel <anderson.zobels@gmail.com>
2022-06-19 18:07:25 -0300

    UPDATE: Componentiza estilo MUI e adiciona marin nos textfield

--- front-end/src/shared/components-pages/registerUser.jsx
+++ front-end/src/shared/components-pages/registerUser.jsx
@@ -11,14 +11,7 @@ import {
 } from '@mui/material';
 import Context from '../contexts/Context';
 import { adminRequestRegister } from '../services/api';
-
-const style = {
-  display: 'flex',
-  flexDirection: 'column',
-  justifyContent: 'center',
-  alignItems: 'center',
-  marginBottom: '1rem',
-};
+import { ContainerTitleText } from '../../style/Styles-MUI'
 
 export default function RegisterUser() {
   const { setUsers } = useContext(Context);
@@ -65,13 +58,11 @@ export default function RegisterUser() {
     setUsers((prevState) => ([...prevState, newUser]));
   };
 
+
   return (
-    <Container
-      style={ style }
-    >
-      <Box
-        component="span"
-        sx={ { p: 2, border: '1px dashed grey', textAlign: 'center' } }
+    <ContainerTitleText>
+      <Box        
+        sx={ { p:1, mr: 'auto', fontSize: '1.5rem' } }
       >
         Cadastrar Novo Usuário
       </Box>
@@ -79,23 +70,29 @@ export default function RegisterUser() {
       <Paper
         sx={ {
           display: 'flex',
-          padding: '1rem',
-          justifyContent: 'space-around',
+          // margin: '1rem',
+          // justifyContent: 'space-around',
           width: '1000px',
+          align: 'center' 
         } }
       >
-        <InputLabel id="seller_select" />
+        <InputLabel id="seller_select"  />
         <TextField
+          sx={ { margin: '1rem' } }
+          size="small"
           inputProps={ { 'data-testid': 'admin_manage__input-name' } }
           variant="filled"
           label="Nome"
           labelId="name_field"
           // value={}
           name="name"
+          
           onChange={ (e) => handleChange(e) }
         />
         <InputLabel id="address_field" />
         <TextField
+          size="small"
+          sx={ { margin: '1rem' } }
           inputProps={ { 'data-testid': 'admin_manage__input-email' } }
           variant="filled"
           label="Email"
@@ -106,6 +103,8 @@ export default function RegisterUser() {
         />
         <InputLabel id="number_field" />
         <TextField
+          sx={ { margin: '1rem' } }
+          size="small"
           inputProps={ {
             'data-testid': 'admin_manage__input-password',
           } }
@@ -118,6 +117,7 @@ export default function RegisterUser() {
           onChange={ (e) => handleChange(e) }
         />
         <NativeSelect
+          sx={ { margin: '1rem' } }
           inputProps={ {
             'data-testid': 'admin_manage__select-role',
           } }
@@ -134,6 +134,8 @@ export default function RegisterUser() {
 
         </NativeSelect>
         <Button
+          variant="contained"
+          sx={ { margin: '1rem' } }
           disabled={ enableButton() }
           onClick={ () => handleClick() }
           data-testid="admin_manage__button-register"
@@ -149,6 +151,6 @@ export default function RegisterUser() {
           Usuario com nome ou email já cadastrado.
         </Alert>
       ) : null}
-    </Container>
+    </ContainerTitleText>
   );
 }
